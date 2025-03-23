26cc4c926bbfb1baa1fc7bce83662e0c0d3a8879
Amit Sinha <>
2025-03-23 18:39:54 +0530

    Handle the Landing Screen

--- sehatdarpan/lib/presentation/screens/splash_screen.dart
+++ sehatdarpan/lib/presentation/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../../core/utils/app_colors.dart';
 import '../../core/utils/app_text_styles.dart';
 import 'onboarding_screen.dart';
@@ -8,38 +9,89 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Image.asset('assets/images/app_logo.png',
-                  width: 150, height: 150),
-            ),
-            Text(
-              'One Platform, Complete Health Solutions',
-              style: AppTextStyles.medium(context: context, color: AppColor.primaryGreen, fontSize: 15),
-            ),
-            const SizedBox(height: 50),
-            // Get Started Button
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)),
-                backgroundColor: Colors.green,
+            Expanded(
+              flex: 2,
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: AppColor.primaryGreen,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: screenHeight * 0.05,
+                        ),
+                        child: Image.asset(
+                          'assets/images/boy_pencil.png',
+                          width: screenWidth * 0.75,
+                        )),
+                  ),
+                ],
               ),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const OnboardingScreen()),
-                );
-              },
-              child: Text('Get Started', style: AppTextStyles.medium(context: context, color: Colors.white),),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/app_logo.png',
+                      width: screenWidth * 0.5,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'We care for your',
+                      style: AppTextStyles.medium(
+                        context: context,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'CHILD',
+                      style: AppTextStyles.bold(
+                        context: context,
+                        fontSize: 32,
+                        color: AppColor.primaryGreen,
+                      ),
+                    ),
+                    Text(
+                      'We care for',
+                      style: AppTextStyles.medium(
+                        context: context,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'STUDENTS',
+                      style: AppTextStyles.bold(
+                        context: context,
+                        fontSize: 28,
+                        color: AppColor.primaryGreen,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
